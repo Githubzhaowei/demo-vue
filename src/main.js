12d2e98229e8c1db4281c70d33f11171aeb2e5b4
Githubzhaowei <44927659+Githubzhaowei@users.noreply.github.com>
2020-09-05 15:21:38 +0800

    添加代码格式化标准
    添加代码格式化标准

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import Axios from 'axios';
+import MintUI from 'mint-ui';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(MintUI);
+Vue.prototype.Axios = Axios;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +16,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
