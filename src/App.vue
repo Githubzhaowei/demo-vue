4b8c47eea7f7aadefc3978b8a86f62dcc5e8a7a8
Githubzhaowei <44927659+Githubzhaowei@users.noreply.github.com>
2020-09-11 18:27:30 +0800

    home页面更新
    home页面更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="外卖">
+        <img slot="icon" src="./assets/index.png" />
+        外卖
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        <img slot="icon" src="./assets/find.png" />
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon" src="./assets/shopcart.png" />
+        发现
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <img slot="icon" src="./assets/vip.png" />
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
@@ -12,12 +28,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import Axios from 'axios';
-import MintUI from 'mint-ui';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+import MintUI from 'mint-ui'
+import '../node_modules/mint-ui/lib/style.min.css'
 
-Vue.config.productionTip = false;
-Vue.use(MintUI);
-Vue.prototype.Axios = Axios;
+Vue.config.productionTip = false
+Vue.use(MintUI)
+Vue.prototype.$axios = Axios
 
 /* eslint-disable no-new */
 new Vue({
@@ -16,4 +17,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-});
+})
